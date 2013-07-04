1b39fad122270d6518ab89ee7a7e884f39d3ac10
AMINE BELHASSINE <belhassine.amine@gmail.com>
2013-07-04 22:16:35 +0100

    Ajout de la soustraction

--- src/Belhassine/HelloBundle/Controller/MathController.php
+++ src/Belhassine/HelloBundle/Controller/MathController.php
@@ -11,4 +11,9 @@ class MathController extends Controller
     {
         return $this->render('BelhassineHelloBundle:Math:addition.html.twig', array('a' => $a,'b' => $b,'somme' => $a+$b));
     }
+    
+    public function soustractionAction(Request $request,$a,$b)
+    {
+        return $this->render('BelhassineHelloBundle:Math:soustraction.html.twig', array('a' => $a,'b' => $b,'somme' => $a-$b));
+    }
 }
